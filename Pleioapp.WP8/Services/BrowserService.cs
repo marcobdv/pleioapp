bed97df758711050c4d002eb1809a2c05dbf6c06
Martijn van Dijk <mhvdijk@gmail.com>
2016-05-25 13:33:20 +0200

    Update packages and add WP8 project

---
+++ Pleioapp.WP8/Services/BrowserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Pleioapp.WP8;
+
+[assembly: Dependency(typeof(BrowserService))]
+namespace Pleioapp.WP8
+{
+
+	public class BrowserService : IBrowserService
+	{
+		public void OpenUrl(string Url) {
+			
+		}
+	}
+}
+
---
+++ Pleioapp.WP8/Services/PushService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Pleioapp.WP8;
+
+[assembly: Dependency(typeof(PushService))]
+namespace Pleioapp.WP8
+{
+	public class PushService : IPushService
+	{
+	    public void RequestToken()
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public void SaveToken(string deviceToken)
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public void SetBadgeNumber(int number)
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public void ProcessPushNotification(Dictionary<string, string> data)
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public string GetToken()
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public Task<bool> RegisterToken()
+	    {
+	        throw new NotImplementedException();
+	    }
+
+	    public Task<bool> DeregisterToken()
+	    {
+	        throw new NotImplementedException();
+	    }
+	}
+}
+
